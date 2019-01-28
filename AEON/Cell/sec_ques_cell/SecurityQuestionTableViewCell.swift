e04e865e931fb53c1dcb59d303bdb5ab003b02e9
khinyadanarthein <khinyadanarthein6107@gmail.com>
2019-01-28 12:00:55 +0630

    initial commit

---
+++ AEON/Cell/sec_ques_cell/SecurityQuestionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SecurityQuestionTableViewCell.swift
+//  AEON
+//
+//  Created by Mobile User on 1/26/19.
+//  Copyright © 2019 AEON microfinance. All rights reserved.
+//
+
+import UIKit
+
+class SecurityQuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
